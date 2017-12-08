7d1bbdb9da114c04e03989eecdf7edfc59d7b7fd
wzhang1984 <wzhang1984@gmail.com>
2017-12-07 16:50:20 -0800

    Add files via upload

--- eQTL/run_qvalue.R
+++ eQTL/run_qvalue.R
@@ -2,19 +2,8 @@
 library(qvalue)
 
 p=read.delim('gene2ncmut_lm_p.txt',header=F,row.names=1)
-qobj=qvalue(p[,4], lambda=seq(0.00,0.99,0.01))
-$qobj=qvalue(p[,4], lambda=seq(0.00,0.99,0.01), pi0.meth='bootstrap')
+qobj=qvalue(p[,4])
 p$q=qobj$qvalues
 p$fdr=p.adjust(p[,4],method='BH')
 write.table(p,'gene2ncmut_lm_p_q.txt',quote=F,sep='\t',col.names=F)
 
-
-
-#library(qvalue)
-#
-#p=read.delim('gene2ncmut_lm_p_allPairs.txt',header=F)
-#qobj=qvalue(p[,4], lambda=seq(0.2,0.8,0.01))
-#p$q=qobj$qvalues
-#p$fdr=p.adjust(p[,4],method='BH')
-#write.table(p,'gene2ncmut_lm_p_allPairs_q.txt',quote=F,sep='\t',col.names=F,row.names=F)
-#
